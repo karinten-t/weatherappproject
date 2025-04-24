1ade1d20b1ff731840ec9d20fbbe46953c42b875
Yazz <itsjustmaitai@gmail.com>
2025-04-24 10:26:06 +0300

    initial add

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import SearchBar from './components/SearchBar';
+import Details from './components/Details';
 
+const API ='e71d7b8ef275c89a7b08ea95b391c447'  
+  
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [search, setSearch] = useState(' ')
+    const [weather, setweather] = useState(null)
+
+
+  const searching =() =>{
+        fetch(`https://api.openweathermap.org/data/2.5/forecast?q=${search}&units=metric&appid=${API}`)
+        .then(res => res.json())
+        .then((result) =>{
+        setweather(result)
+        })}
+
+return (
+    <>
+    <SearchBar search ={search} setSearch={setSearch} onSearch={searching}/>
+    <Details weather={weather}/>
+  
+  </>
+  )
 }
 
-export default App;
+export default App
